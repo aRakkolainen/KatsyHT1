920233bc1160a397d35dce63bbe08383b2af7f78
aRakkolainen <aino.rakkolainen@gmail.com>
2023-07-04 19:41:34 +0300

    main program added

---
+++ reverse.c
@@ -0,0 +1,11 @@
+//Harjoitusprojekti 1: Warming up program: Reverse.c
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	//Writing to input.txt file: 
+	
+
+	//Reading input.txt reverse and writing to output.txt
+
+	return(0);
+}
