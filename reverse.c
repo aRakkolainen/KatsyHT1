9cd95526af3d5ddee6c01793e2458b97a781b9c5
aRakkolainen <aino.rakkolainen@gmail.com>
2023-07-04 19:48:16 +0300

    new version

--- reverse.c
+++ reverse.c
@@ -1,7 +1,10 @@
 //Harjoitusprojekti 1: Warming up program: Reverse.c
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char * argv[]) {
+	if (argc == 0) {
+		printf("No command-line arguments given\n");
+	}
 	//Writing to input.txt file: 
 	
 
