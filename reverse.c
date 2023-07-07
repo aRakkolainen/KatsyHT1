3d56117f131a78c9c54bc09fe33c82cefc7efd12
aRakkolainen <aino.rakkolainen@gmail.com>
2023-07-07 15:03:18 +0300

    input and output files added

--- reverse.c
+++ reverse.c
@@ -24,19 +24,35 @@ int main(int argc, char * argv[]) {
 		strcpy(outputFileName, argv[2]);
 		FILE *inputFile;
 		FILE *outputFile;
+
 		// Opening the input file 
 		if ((inputFile = fopen(inputFileName, "r")) == NULL) {
-			perror("Failed to open the file");
+			fprintf(stderr, "error: cannot open file '%s'\n", inputFileName);
 			exit(1);
 		} else {
+			if((outputFile = fopen(outputFileName, "w")) == NULL) {
+				fprintf(stderr, "error: cannot open file '%s'\n", outputFileName);
+			}
+			//How to use getline() function: https://c-for-dummies.com/blog/?p=5445 
 			char *line=NULL;
-			size_t len =0; 
-			getline(&line, &len,inputFile);
+			size_t len;
+			int read; 
+			int lineCounter=0; 
+			//char lines[];
+			do {
+				read = getline(&line, &len, inputFile);
+				if (read != -1 ) {
+					printf("%s", line);
+					//fprintf(outputFile, line);
+					lineCounter++; 
+				}
+			} while (read > 1);
+			printf("Lines in the file: %d\n", lineCounter);
 
 			free(line); 
 			fclose(inputFile);
+
 		}
-		outputFile = fopen(outputFileName, "w");
 		
 
 	}
